4b0aa5792796ae9bad570347e0d1a8455e7b6320
Andrey Potapov <aapot@ya.ru>
2024-01-14 16:02:12 +0300

    реализован вывод на главную страницу

--- home/views.py
+++ home/views.py
@@ -1,6 +1,19 @@
+from django.db.models import Q
 from django.shortcuts import render
 from django.views import generic
+
+from blog.models import Post
+from customer.models import Customer
+from mailing.models import Mailing
 
 
 class IndexView(generic.TemplateView):
     template_name = 'home/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['total_mailings'] = Mailing.objects.all().count()
+        context['active_mailings'] = Mailing.objects.filter(Q(status='created') | Q(status='in progress')).count()
+        context['unique_customers'] = Customer.objects.order_by('email').distinct('email').count()
+        context['random_posts'] = Post.objects.order_by('?')[:3]
+        return context
